f1ec29d93b7190e09a4d93e8e95674fbf99ced6c
DesktopMetal-JS <joeshim@desktopmetal.com>
2016-07-24 17:55:26 -0400

    Enabled Bed
    During the original setup I forgot to enable the use of a heated bed.
    This is now enabled. -Shjmy

--- Marlin-RC 1v1/Marlin/Configuration.h
+++ Marlin-RC 1v1/Marlin/Configuration.h
@@ -186,7 +186,7 @@
 #define TEMP_SENSOR_1 1 //changed 23JUL2016 -Shjmy
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
-#define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_BED 1
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
 //#define TEMP_SENSOR_1_AS_REDUNDANT
